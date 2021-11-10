5ef3de7693a064baf64ac5f1671c46307ed3842e
fprebolledo <fprebolledo@uc.cl>
2021-11-10 20:40:59 -0300

    feat(): add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :movies, only: [:new, :create, :show]
+  resources :movie_shows, only: [:new, :create, :show]
 end
