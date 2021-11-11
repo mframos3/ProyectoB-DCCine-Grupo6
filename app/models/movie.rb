d2d55895899f76879bc19d558a87fcbd33b6addd
fprebolledo <fprebolledo@uc.cl>
2021-11-10 21:03:56 -0300

    feat(movies): add image to movies model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
 	has_many :movie_shows
+	has_one_attached :image
+	validates_presence_of :title, :message => "Nombre debe estar presente"
+	validates_presence_of :image, :message => "Imagen debe estar presente"
 end
