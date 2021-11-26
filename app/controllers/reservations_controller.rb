04569434d9dd4586b3c2151d761b956ed91cb53a
egromero <egromero@uc.cl>
2021-11-26 15:58:37 -0300

    (reservation): add validations to reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,24 +7,27 @@ class ReservationsController < ApplicationController
     def create_reservation
         seats = params[:seats]
         unless seats
-            redirect_to movies_path, alert: "Reserva no realizada, ninguna silla fue seleccionada"
+            return redirect_back fallback_location: root_path, alert: "Reserva no realizada, ninguna silla fue seleccionada"
         end
         if params[:user_name].empty?
-            redirect_to movies_path, alert: "Reserva no realizada, se necesita un usuario para reservar."
+            return redirect_back fallback_location: root_path, alert: "Reserva no realizada, se necesita un usuario para reservar."
         end
-        user = User.create(name: params[:user_name], rut: params[:user_rut])
+        user = User.create(name: params[:user_name])
         if user.save
             seats.each do |seat|
                 seat = Seat.find(seat)
+                if(seat.user_id != nil) 
+                    return redirect_back fallback_location: root_path, alert: "Reserva no realizada, Uno a mas asientos ya están reservados."    
+                end
                 seat.user_id = user.id
                 unless seat.save
-                    redirect_to movies_path, alert: "Reserva no realizada, ha ocurrido un error creando la reserva"    
+                    return redirect_back fallback_location: root_path, alert: "Reserva no realizada, ha ocurrido un error creando la reserva"    
                 end
             end
         else
-            redirect_to movies_path, alert: "Reserva no realizada, ha ocurrido un error creando la reserva"
+            return redirect_back fallback_location: root_path, alert: "Reserva no realizada, ha ocurrido un error creando la reserva"
         end  
-        redirect_to movies_path, notice: "Reserva creada exitosamente!"
+        return redirect_back fallback_location: root_path, notice: "Reserva creada exitosamente!"
     end
 
     def show
